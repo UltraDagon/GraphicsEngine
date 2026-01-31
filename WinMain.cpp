78897323907854b7861e7e708ca248f7391b57e6
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2026-01-31 01:02:18 -0600

    Window now closes and can return with custom status codes

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,18 +1,30 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		// Exit application if window is closed
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
-	int nCmdShow)
+	int nShowCmd)
 {
-
 	const auto pClassName = L"HelloWorld";
 	// Register window class
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -36,6 +48,22 @@ int CALLBACK WinMain(
 	// Show window
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true);
-	return 0;
+	// Message pump
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	// Return -1 if error, otherwise return window exit status
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
